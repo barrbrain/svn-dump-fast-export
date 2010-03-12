de583fc76359f33d441634b088158f396289ec97
David Barr <david.barr@cordelta.com>
2010-03-12 22:05:27 +1100

    Added a simple string pool implementation.
    The purpose is to assign incremental integer ID's to unique strings.
    I intend to use it to create a compact data structure that represents the subversion tree.
    
    Signed-off-by: David Barr <david.barr@cordelta.com>

---
+++ string_pool.c
@@ -0,0 +1,117 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2010 David Barr <david.barr@cordelta.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice(s), this list of conditions and the following disclaimer
+ *    unmodified other than the allowable addition of one or more
+ *    copyright notices.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice(s), this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#define NDEBUG
+#include <assert.h>
+#include "trp.h"
+#include "string_pool.h"
+
+typedef struct node_s node_t;
+
+struct node_s {
+    uint32_t offset;
+    trp_node(node_t) children;
+};
+
+typedef trp(node_t) tree_t;
+
+struct pool_s {
+    uint32_t size;
+    uint32_t entries;
+    char *data;
+    node_t *index;
+    tree_t tree;
+};
+
+static struct pool_s *pool;
+
+static char*
+node_value(node_t *node) {
+    return &(pool->data[node->offset]);
+}
+
+static int
+node_value_cmp(node_t *a, node_t *b) {
+    return strcmp(node_value(a), node_value(b));
+}
+
+static int
+node_indentity_cmp(node_t *a, node_t *b) {
+    int r = node_value_cmp(a, b);
+    return r ? r : (((uintptr_t) a) > ((uintptr_t) b))
+	  - (((uintptr_t) a) < ((uintptr_t) b));
+}
+
+trp_gen(static, tree_, tree_t, node_t, children, pool->index, node_indentity_cmp);
+
+void
+pool_init(uint32_t max_size, uint32_t max_entries) {
+    pool = malloc(sizeof(*pool));
+    pool->data = malloc(max_size);
+    pool->size = 0;
+    pool->index = malloc(max_entries * sizeof(node_t));
+    // First entry is reserved for NULL
+    pool->entries = 1;
+    tree_new(&pool->tree, 42);
+}
+
+char* 
+pool_fetch(uint32_t entry) {
+    return node_value(&pool->index[entry]);
+}
+
+uint32_t
+pool_intern(char* key) {
+    node_t *node = &pool->index[pool->entries];
+    node_t *match = NULL;
+    node->offset = pool->size;
+    strcpy(node_value(node), key);
+    match = tree_psearch(&pool->tree, node);
+    if(!match || node_value_cmp(node, match)) {
+       tree_insert(&pool->tree, node);
+       pool->size += strlen(key) + 1;
+       return pool->entries++;
+    } else {
+       return match - pool->index;
+    }
+}
+
+uint32_t
+pool_tok_r(char *str, const char *delim, char **saveptr) {
+    char * token = strtok_r(str, delim, saveptr);
+    return token ? pool_intern(token) : 0;
+}
