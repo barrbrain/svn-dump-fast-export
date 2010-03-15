96dc35bfd7e643591890cb8f60c857485aa3c33d
David Barr <david.barr@cordelta.com>
2010-03-15 21:57:24 +1100

    Generated some test data to ease verifying the core.
    This dataset was generated from a real repository and sanitised
    to remove any textual content.
    Commands are of the form:
    R n -- commit revision n
    M a -- modify path a
    D a -- delete path a
    C n:a b -- copy path a from revison n to path b
    
    Signed-off-by: David Barr <david.barr@cordelta.com>

---
+++ sanitise.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "string_pool.h"
+
+int main(int argc, char **argv)
+{
+    char *cmd;
+    char *a1, *a2, *a3;
+    char buffer[4096];
+    pool_init(300000, 30000);
+    for (fgets(buffer, 4096, stdin);
+         !feof(stdin); fgets(buffer, 4096, stdin)) {
+
+        cmd = strtok(buffer, " ");
+        if (!strcmp(cmd, "copy")) {
+            a1 = strtok(NULL, ":");
+            a2 = strtok(NULL, "\"");
+            strtok(NULL, "\"");
+            a3 = strtok(NULL, "\"");
+            printf("%s %s:\"", cmd, a1);
+            for (a2 = strtok(a2, "/"); a2;
+                 (a2 = strtok(NULL, a2)) && putchar('/')) {
+                printf("%d", pool_intern(a2));
+            }
+            printf("\" \"");
+            for (a3 = strtok(a3, "/"); a3;
+                 (a3 = strtok(NULL, a3)) && putchar('/')) {
+                printf("%d", pool_intern(a3));
+            }
+            printf("\"\n");
+        } else if (!strcmp(cmd, "modify") || !strcmp(cmd, "delete")) {
+            a1 = strtok(NULL, "\"");
+            strtok(NULL, "\"");
+            printf("%s \"", cmd);
+            for (a1 = strtok(a1, "/"); a1;
+                 (a1 = strtok(NULL, a1)) && putchar('/')) {
+                printf("%d", pool_intern(a1));
+            }
+            printf("\"\n");
+        } else if (!strcmp(cmd, "commit")) {
+            a1 = strtok(NULL, "\n");
+            printf("%s %s\n", cmd, a1);
+        }
+    }
+    return 0;
+}
