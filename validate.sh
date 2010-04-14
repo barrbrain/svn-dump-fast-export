b3a17de47f44e0188e85c8b294451931c28b0186
David Barr <david.barr@cordelta.com>
2010-04-15 02:16:18 +1000

    Simple validation script with hashification.
    This script depends on SVK for fast checkout and update.
    Revisions are walked at the root of the repository.
    After each update new files are hardlinked to equivalent files.
    
    When the final revision is reached, the repository is imported.
    The revisions are then walked in reverse and git reset confirms.
    
    Signed-off-by: David Barr <david.barr@cordelta.com>

---
+++ validate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+SVK_DEPOT=""
+URL="example.com"
+CO_DIR=validation
+HASH_DIR=hashes
+MAX_REV=23000
+
+mkdir $CO_DIR
+svk co -r1 /$SVK_DEPOT/ $CO_DIR
+mkdir -p $HASH_DIR
+for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
+  svk up -r$REV $CO_DIR
+  # Hashify working copy
+  find $CO_DIR -type f -links 1 -exec shasum '{}' + | (
+    while read HASH FILE ; do
+      [ -x "$FILE" ] && HASH="$HASH"x
+      ln "$FILE" $HASH_DIR/$HASH 2>/dev/null || \
+        ln -f $HASH_DIR/$HASH "$FILE"
+    done
+  )
+done
+
+( cd $CO_DIR
+  git init
+  svk admin dump -q /$SVK_DEPOT/ | \
+    ./svn-dump-fast-export "$URL" | \
+    git fast-import
+  git rev-list master | (
+    while read HASH ; do
+      REV=`git log -1 --pretty=format:%b $HASH | \
+        grep ^git-svn-id | \
+        sed 's/^[^@]*@\([0-9]*\) .*$/\1/'`
+      [ -z "$REV" ] && continue
+      echo $REV $HASH
+      svk up -r$REV || continue
+      svk revert -R .
+      MISMATCH=`git reset $HASH | wc -l`
+      [ $MISMATCH -gt 0 ] && echo MISMATCHES: $MISMATCH && break
+    done
+  ) | tee ../validate.log
+)
