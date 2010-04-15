34e24c54d3e3fb23aa80b93afad897bbb4d74696
David Barr <david.barr@cordelta.com>
2010-04-15 16:58:03 +1000

    Reimplement hashing using git.
    Signed-off-by: David Barr <david.barr@cordelta.com>

--- validate.sh
+++ validate.sh
@@ -6,21 +6,28 @@ HASH_DIR=hashes
 MAX_REV=23000
 
 svk co -r1 /$SVK_DEPOT/ $CO_DIR
+( cd $CO_DIR ; git init )
 mkdir -p $HASH_DIR/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}
 for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
   svk up -r$REV $CO_DIR
   # Hashify working copy
-  find $CO_DIR -type d -amin +5 -prune -o \
-    -type f -links 1 -exec shasum '{}' + | (
+  ( cd $CO_DIR ; git update-index --refresh )
+  ( cd $CO_DIR ; git ls-files -moz |  xargs -0 shasum ) \
+    | (
     while read HASH FILE ; do
+      FILE="$CO_DIR/$FILE"
       [ -x "$FILE" ] && HASH="$HASH"x
       HASH_FILE="$HASH_DIR/${HASH:0:2}/$HASH"
       ln "$FILE" "$HASH_FILE" 2>/dev/null || \
         ln -f "$HASH_FILE" "$FILE"
     done
   )
+  ( cd $CO_DIR ;
+    git ls-files -dz | xargs -0 git update-index --info-only --remove 
+    git ls-files -moz | xargs -0 git update-index --info-only --add --replace 
+  )
 done
-
+exit 0
 ( cd $CO_DIR
   git init
   svk admin dump /$SVK_DEPOT/ | \
