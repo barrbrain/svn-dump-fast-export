bf6e9fde2559a2289b931539b59e8d2fdce3db41
David Barr <david.barr@cordelta.com>
2010-04-21 20:20:23 +1000

    Rewrite so that git-ls-files only needs to examine the index.
    Signed-off-by: David Barr <david.barr@cordelta.com>

--- replay.sh
+++ replay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SVK_DEPOT=""
-CO_DIR=validation
+CO_DIR=unchanged
 HASH_DIR=.git/wc
 
 SVN_UUID=`svk pg --revprop -r0 svn:sync-from-uuid /$SVK_DEPOT/`
@@ -18,10 +18,22 @@ mkdir -p $HASH_DIR/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}\
 {0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}
 for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
   svk up -r$REV | tee .git/svklog
+  # Process SVK log
+  grep '^A' .git/svklog | sed 's/^....//' | tr '\n' '\0' \
+   | xargs -0 -J % find % -maxdepth 0 -not -type d -print0 > .git/svnadd
+  grep '^U' .git/svklog | sed 's/^....//' | tr '\n' '\0' > .git/svnupdate
+  grep '^D' .git/svklog | sed 's/^....//' | tr '\n' '\0' > .git/svndelete
+  # Add new files
+  git update-index -z --add --replace --stdin < .git/svnadd
+  # Clear skip-worktree for updated and deleted files
+  cat .git/svn{update,delete} | xargs -0 git ls-files -z > .git/filelist
+  git update-index -z --no-skip-worktree --stdin < .git/filelist
+  # Modify index with updated and removed files
+  git update-index -z --remove --stdin < .git/filelist
+  # Set skip-worktree for added and updated files
+  cat .git/svn{add,update} > .git/filelist
+  git update-index -z --skip-worktree --stdin < .git/filelist
   # Hashify working copy
-  grep '[A-Z]   ' .git/svklog | sed 's/^....//' \
-   | xargs git ls-files -modz | tee .git/filelist \
-   | git update-index -z --add --replace --remove --stdin
   xargs -0 git ls-files -s < .git/filelist | (
     while read MODE HASH STAGE FILE ; do
       HASH_FILE="$HASH_DIR/${HASH:0:2}/$HASH$MODE"
@@ -29,6 +41,7 @@ for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
         ln -f "$HASH_FILE" "$FILE"
     done
   )
+  # Extract commit data from SVK
   SVN_AUTHOR=`svk pg --revprop -r$REV svn:author`
   SVN_DATE=`svk pg --revprop -r$REV svn:date`
   export GIT_COMMITTER_NAME=$SVN_AUTHOR
@@ -38,6 +51,7 @@ for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
   export GIT_AUTHOR_NAME="$GIT_COMMITTER_NAME"
   export GIT_AUTHOR_EMAIL="$GIT_COMMITTER_EMAIL"
   export GIT_AUTHOR_DATE="$GIT_COMMITTER_DATE"
+  # Write tree, commit and advance HEAD
   GIT_PARENT=""
   [ $REV -gt 1 ] && GIT_PARENT="-p `git show-ref --head -s HEAD`"
   GIT_TREE=`git write-tree`
@@ -46,5 +60,5 @@ for (( REV=1 ; REV<=MAX_REV ; ++REV )) do
    | git commit-tree $GIT_TREE $GIT_PARENT`
   git update-ref HEAD $GIT_COMMIT
   git tag $REV
-  git gc --auto
+  [[ "$REV" =~ "00$$" ]] && git gc --auto
 done
