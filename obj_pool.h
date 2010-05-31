9cdd6a4ad299733edf3e80d2d800f4767b51846f
Ramkumar Ramachandra <artagnon@gmail.com>
2010-05-31 11:50:34 +0200

    Restyle obj_pool.h
    Indent using TAB and move the location of the line breaks in
    obj_pool.h.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- obj_pool.h
+++ obj_pool.h
@@ -15,49 +15,48 @@
  *   intial_capacity  : The initial size of the memory pool (ex: 4096).
  *
  */
-#define obj_pool_gen(pre, obj_t, initial_capacity)                         \
-static struct {                                                            \
-    uint32_t size;                                                         \
-    uint32_t capacity;                                                     \
-    obj_t *base;                                                           \
-} pre##_pool = { 0, 0, NULL};                                              \
-static uint32_t pre##_alloc(uint32_t count)                                \
-{                                                                          \
-    uint32_t offset;                                                       \
-    if (pre##_pool.size + count > pre##_pool.capacity) {                   \
-        while (pre##_pool.size + count > pre##_pool.capacity) {            \
-            if (pre##_pool.capacity) {                                     \
-                pre##_pool.capacity *= 2;                                  \
-            } else {                                                       \
-                pre##_pool.capacity = initial_capacity;                    \
-            }                                                              \
-        }                                                                  \
-        pre##_pool.base =                                                  \
-            realloc(pre##_pool.base, pre##_pool.capacity * sizeof(obj_t)); \
-    }                                                                      \
-    offset = pre##_pool.size;                                              \
-    pre##_pool.size += count;                                              \
-    return offset;                                                         \
-}                                                                          \
-static void pre##_free(uint32_t count)                                     \
-{                                                                          \
-    pre##_pool.size -= count;                                              \
-}                                                                          \
-static uint32_t pre##_offset(obj_t *obj)                                   \
-{                                                                          \
-    return obj == NULL ? ~0 : obj - pre##_pool.base;                       \
-}                                                                          \
-static obj_t *pre##_pointer(uint32_t offset)                               \
-{                                                                          \
-    return offset >= pre##_pool.size ? NULL : &pre##_pool.base[offset];    \
-}                                                                          \
-static void pre##_reset(void)                                              \
-{                                                                          \
-    if (pre##_pool.base)                                                   \
-        free(pre##_pool.base);                                             \
-    pre##_pool.base = NULL;                                                \
-    pre##_pool.size = 0;                                                   \
-    pre##_pool.capacity = 0;                                               \
-}                                                                          \
+#define obj_pool_gen(pre, obj_t, initial_capacity) \
+static struct { \
+	uint32_t size; \
+	uint32_t capacity; \
+	obj_t *base; \
+} pre##_pool = { 0, 0, NULL}; \
+static uint32_t pre##_alloc(uint32_t count) \
+{ \
+	uint32_t offset; \
+	if (pre##_pool.size + count > pre##_pool.capacity) { \
+		while (pre##_pool.size + count > pre##_pool.capacity) \
+			if (pre##_pool.capacity) \
+				pre##_pool.capacity *= 2; \
+			else \
+				pre##_pool.capacity = initial_capacity; \
+		pre##_pool.base = \
+			realloc(pre##_pool.base, \
+				pre##_pool.capacity * sizeof(obj_t)); \
+	} \
+	offset = pre##_pool.size; \
+	pre##_pool.size += count; \
+	return offset; \
+} \
+static void pre##_free(uint32_t count) \
+{ \
+	pre##_pool.size -= count; \
+} \
+static uint32_t pre##_offset(obj_t *obj) \
+{ \
+	return obj == NULL ? ~0 : obj - pre##_pool.base; \
+} \
+static obj_t *pre##_pointer(uint32_t offset) \
+{ \
+	return offset >= pre##_pool.size ? NULL : &pre##_pool.base[offset]; \
+} \
+static void pre##_reset(void) \
+{ \
+	if (pre##_pool.base) \
+		free(pre##_pool.base); \
+	pre##_pool.base = NULL; \
+	pre##_pool.size = 0; \
+	pre##_pool.capacity = 0; \
+}
 
 #endif
