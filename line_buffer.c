c2132c3f7b72ccd8fb4231557d9b0bd37f7d36af
Jonathan Nieder <jrnieder@gmail.com>
2010-06-24 18:30:05 -0500

    line_buffer: Simplify by removing push-back buffer
    Nobody ungets characters, so let stdio take care of buffering.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- line_buffer.c
+++ line_buffer.c
@@ -18,43 +18,26 @@ obj_pool_gen(blob, char, 4096);
 
 static char line_buffer[LINE_BUFFER_LEN];
 static char byte_buffer[COPY_BUFFER_LEN];
-static uint32_t line_buffer_len = 0;
-static uint32_t line_len = 0;
 
+/* Read a line without trailing newline. */
 char *buffer_read_line(void)
 {
 	char *end;
-	uint32_t n_read;
 
-	if (line_len) {
-		memmove(line_buffer, &line_buffer[line_len],
-		                line_buffer_len - line_len);
-		line_buffer_len -= line_len;
-		line_len = 0;
-	}
-
-	end = memchr(line_buffer, '\n', line_buffer_len);
-	while (line_buffer_len < LINE_BUFFER_LEN - 1 &&
-		   !feof(stdin) && !ferror(stdin) && NULL == end) {
-		n_read = fread(&line_buffer[line_buffer_len], 1,
-				LINE_BUFFER_LEN - 1 - line_buffer_len,
-				stdin);
-		end = memchr(&line_buffer[line_buffer_len], '\n', n_read);
-		line_buffer_len += n_read;
-	}
-
-	if (ferror(stdin))
+	if (!fgets(line_buffer, sizeof(line_buffer), stdin))
+		/* Error or data exhausted. */
 		return NULL;
-
-	if (end != NULL) {
-		line_len = end - line_buffer;
-		line_buffer[line_len++] = '\0';
-	} else {
-		line_len = line_buffer_len;
-		line_buffer[line_buffer_len] = '\0';
-	}
-
-	if (line_len == 0)
+	end = line_buffer + strlen(line_buffer);
+	if (end[-1] == '\n')
+		end[-1] = '\0';
+	else if (feof(infile))
+		; /* No newline at end of file.  That's fine. */
+	else
+		/*
+		 * Line was too long.
+		 * There is probably a saner way to deal with this,
+		 * but for now let's return an error.
+		 */
 		return NULL;
 
 	return line_buffer;
@@ -65,31 +48,13 @@ char *buffer_read_string(uint32_t len)
 	char *s;
 	blob_free(blob_pool.size);
 	s = blob_pointer(blob_alloc(len + 1));
-	uint32_t offset = 0;
-	if (line_buffer_len > line_len) {
-		offset = line_buffer_len - line_len;
-		if (offset > len)
-			offset = len;
-		memcpy(s, &line_buffer[line_len], offset);
-		line_len += offset;
-	}
-	if (offset < len)
-		offset += fread(&s[offset], 1, len - offset, stdin);
-	s[offset] = '\0';
-	return s;
+	s[fread(s, 1, len, stdin)] = '\0';
+	return ferror(stdin) ? NULL : s;
 }
 
 void buffer_copy_bytes(uint32_t len)
 {
 	uint32_t in;
-	if (line_buffer_len > line_len) {
-		in = line_buffer_len - line_len;
-		if (in > len)
-			in = len;
-		fwrite(&line_buffer[line_len], 1, in, stdout);
-		len -= in;
-		line_len += in;
-	}
 	while (len > 0 && !feof(stdin)) {
 		in = len < COPY_BUFFER_LEN ? len : COPY_BUFFER_LEN;
 		in = fread(byte_buffer, 1, in, stdin);
@@ -101,13 +66,6 @@ void buffer_copy_bytes(uint32_t len)
 void buffer_skip_bytes(uint32_t len)
 {
 	uint32_t in;
-	if (line_buffer_len > line_len) {
-		in = line_buffer_len - line_len;
-		if (in > len)
-			in = len;
-		line_len += in;
-		len -= in;
-	}
 	while (len > 0 && !feof(stdin) && !ferror(stdin)) {
 		in = len < COPY_BUFFER_LEN ? len : COPY_BUFFER_LEN;
 		in = fread(byte_buffer, 1, in, stdin);
