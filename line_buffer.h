4c23922c24fb329c49f90ef9bb4095ec597b218f
Jonathan Nieder <jrnieder@gmail.com>
2010-10-10 21:39:21 -0500

    vcs-svn: Collect line_buffer data in a struct
    Prepare for the line_buffer lib to support input from multiple files,
    by collecting global state in a struct that can be easily passed around.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- line_buffer.h
+++ line_buffer.h
@@ -1,7 +1,16 @@
 #ifndef LINE_BUFFER_H_
 #define LINE_BUFFER_H_
 
-#include <stdint.h>
+#include "strbuf.h"
+
+#define LINE_BUFFER_LEN 10000
+
+struct line_buffer {
+	char line_buffer[LINE_BUFFER_LEN];
+	struct strbuf blob_buffer;
+	FILE *infile;
+};
+#define LINE_BUFFER_INIT {"", STRBUF_INIT, NULL}
 
 int buffer_init(const char *filename);
 int buffer_deinit(void);
