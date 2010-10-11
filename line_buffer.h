af8a1a712c472a734f1e44eda6a9cd84c258283d
Jonathan Nieder <jrnieder@gmail.com>
2010-10-10 21:44:21 -0500

    vcs-svn: Make buffer_skip_bytes() report partial reads
    Tell the caller how many bytes were actually skipped.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- line_buffer.h
+++ line_buffer.h
@@ -17,7 +17,7 @@ int buffer_deinit(struct line_buffer *buf);
 char *buffer_read_line(struct line_buffer *buf);
 char *buffer_read_string(struct line_buffer *buf, uint32_t len);
 void buffer_copy_bytes(struct line_buffer *buf, uint32_t len);
-void buffer_skip_bytes(struct line_buffer *buf, uint32_t len);
+uint32_t buffer_skip_bytes(struct line_buffer *buf, uint32_t len);
 void buffer_reset(struct line_buffer *buf);
 
 #endif
